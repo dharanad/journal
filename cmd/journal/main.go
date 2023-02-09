958a9c76cd746e0d495778d71e4e8a567757c532
Dharan Aditya <dharan.aditya@machinemax.com>
2023-02-10 02:37:13 +0530

    Implement web server

--- cmd/journal/main.go
+++ cmd/journal/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello, journal")
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatal("Error setting up zap logger", err)
+	}
+	sugar := logger.Sugar()
+	sugar.Info("Starting journal")
+
+	r := gin.Default()
+	r.GET("/ping", PingHandler())
+	if err = r.Run(":80"); err != nil {
+		sugar.Fatal("Error running http-server", err)
+	}
+}
+
+func PingHandler() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	}
 }
